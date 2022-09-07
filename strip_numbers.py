7cb83b46a9a2759072262130a892fc33e60a11e7
masiarek <masiarek@gmail.com>
2022-09-07 10:29:59 -0400

    add numbers

--- strip_numbers.py
+++ strip_numbers.py
@@ -1,1 +1,13 @@
-def strip_n(data):
+import string
+
+data = """
+aaa1, aa1, aaaa1
+a2, aaa2, aaa2"""
+
+
+def strip_all_number(data):
+    return data.strip(string.digits)
+
+
+new_data = strip_all_number(data)
+print(new_data)
