bdbed711b5f68a61173397330d455a4bd6c287c5
Adam Masiarek <masiarek@gmail.com>
2023-06-24 16:49:11 -0400

    thuis is comment

--- strip_numbers.py
+++ strip_numbers.py
@@ -1,14 +1,17 @@
 import string
-import regex as re
 
-data = """
-aaa1, aa1, aaaa1
-a2, aaa2, aaa2"""
+# import regex as re
+# adam test with Ed
+# data = """
+# aaa1, aa1, aaaa1
+# a2, aaa2, aaa2"""
+#
+#
+# def strip_all_number(data):
+#     p = re.compile(r"\d")
+#     return re.sub(p, "", data)
+#
+#
+# print(strip_all_number(data))
 
-
-def strip_all_number(data):
-    p = re.compile(r"\d")
-    return re.sub(p, "", data)
-
-
-print(strip_all_number(data))
+print('aaa')
